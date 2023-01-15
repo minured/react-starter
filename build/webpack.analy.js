6bbd81b5c43df44b05361dc816de6f2472508277
minured <minured@qq.com>
2023-01-16 00:24:56 +0800

    分析构建包

--- build/webpack.analy.js
+++ build/webpack.analy.js
@@ -2,12 +2,15 @@ const SpeedMeasurePlugin = require("speed-measure-webpack-plugin");
 const { Configuration } = require("webpack");
 const { merge } = require("webpack-merge");
 const prodConfig = require("./webpack.prod");
+const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 
 const smp = new SpeedMeasurePlugin();
 
 /**
  * @type {Configuration}
  */
-const analyzeConfig = {};
+const analyzeConfig = {
+    plugins: [new BundleAnalyzerPlugin()],
+};
 
 module.exports = smp.wrap(merge(prodConfig, analyzeConfig));
