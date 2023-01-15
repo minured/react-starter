49058f90680bdaad77ea6d124c4b4a6c04217fdd
minured <minured@qq.com>
2023-01-16 02:31:55 +0800

    懒加载

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,17 @@
-import React from "react";
+import React, { useState, lazy, Suspense } from "react";
 import "@/app.less";
 import ClassCom from "@/demo/DecoratorCom";
 import amber from "@/assets/images/amber.png";
 import officialSite from "@/assets/images/official-site.png";
 
+// 会被单独打包
+const LazyComponent = lazy(() => import("@/demo/LazyCom"));
+
 const App = () => {
     console.log(process.env.NODE_ENV);
     console.log(process.env.APP_ENV);
+
+    const [visible, setVisible] = useState(false);
 
     return (
         <>
@@ -16,6 +21,20 @@ const App = () => {
             <img src={amber} alt="amber" width={500} />
             <p>小于10kb的图片转换成base64</p>
             <img src={officialSite} alt="" />
+            <button
+                onClick={() => {
+                    // 单独打包的css
+                    import("./demo/lazy.less");
+                    setVisible(!visible);
+                }}
+            >
+                load component
+            </button>
+            {visible && (
+                <Suspense>
+                    <LazyComponent />
+                </Suspense>
+            )}
         </>
     );
 };
---
+++ src/demo/LazyCom.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+
+
+const LazyComponent:React.FC =() => {
+    return (
+        <div>
+            I am a lazy loaded component.
+        </div>
+    )
+}
+
+export default LazyComponent
